bf68275cbde11682973a388e2844659e2e6f3aa5
Bagaskara Wisnu Gunawan <bagaswisnu07@gmail.com>
2020-12-02 02:50:19 +0700

    feat: testimonial crud

--- app/Http/Controllers/TestimonialController.php
+++ app/Http/Controllers/TestimonialController.php
@@ -2,9 +2,66 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Testimonial;
 use Illuminate\Http\Request;
 
 class TestimonialController extends Controller
 {
-    //
+    public function index()
+    {
+        $testimonials = Testimonial::paginate();
+
+        return view('testimonials.index', compact('testimonials'));
+    }
+
+    public function create()
+    {
+        return view('testimonials.create');
+    }
+
+    public function store(Request $request)
+    {
+        $attributes = $this->validate($request, [
+            'name' => ['required'],
+            'content' => ['required'],
+            'image' => ['required', 'image'],
+        ]);
+
+        $attributes['image'] = $request->file('image')->storeAs('testimonials', md5(time()), 'public');
+
+        Testimonial::create($attributes);
+
+        $request->session()->flash('success', 'Berhasil menambahkan testimonial.');
+        return redirect()->route('testimonials.index');
+    }
+
+    public function edit(Testimonial $testimonial)
+    {
+        return view('testimonials.edit', compact('testimonial'));
+    }
+
+    public function update(Request $request, Testimonial $testimonial)
+    {
+        $attributes = $this->validate($request, [
+            'name' => ['required'],
+            'content' => ['required'],
+            'image' => ['nullable', 'image'],
+        ]);
+
+        if ($request->hasFile('image')) {
+            $attributes['image'] = $request->file('image')->storeAs('testimonials', md5(time()), 'public');
+        }
+
+        $testimonial->update($attributes);
+
+        $request->session()->flash('success', 'Berhasil memperbarui testimonial.');
+        return redirect()->route('testimonials.index');
+    }
+
+    public function destroy(Testimonial $testimonial)
+    {
+        $testimonial->delete();
+
+        return redirect()->route('testimonials.index');
+    }
 }
---
+++ resources/views/testimonials/index.blade.php
@@ -0,0 +1,81 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="float-left font-semibold text-xl text-gray-800 leading-tight">
+            Testimonial
+        </h2>
+        <div class="float-right">
+            <a href="{{ route('testimonials.create') }}" class="px-2 py-2 rounded-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50">{{ __('Create') }}</a>
+        </div>
+        <div class="clearfix"></div>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="flex flex-col">
+                <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8 mb-2">
+                    <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                        <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                            <table class="min-w-full divide-y divide-gray-200">
+                                <thead>
+                                    <tr>
+                                        <th scope="col"
+                                            class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                            {{ __("Nama") }}
+                                        </th>
+                                        <th scope="col"
+                                            class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                            {{ __("Konten") }}
+                                        </th>
+                                        <th scope="col" class="px-6 py-3 bg-gray-50">
+                                            <span class="sr-only">{{ __("Actions") }}</span>
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody class="bg-white divide-y divide-gray-200">
+                                    @forelse($testimonials as $testimonial)
+                                        <tr>
+                                            <td class="px-6 py-4 whitespace-nowrap">
+                                                <div class="flex items-center">
+                                                    <div class="flex-shrink-0 h-10 w-10">
+                                                        <img class="h-10 w-10 rounded-full"
+                                                            src="{{ asset($testimonial->image) }}"
+                                                            alt="Foto">
+                                                    </div>
+                                                    <div class="ml-4">
+                                                        <div class="text-sm text-gray-900">
+                                                            {{ $testimonial->name }}
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </td>
+                                            <td class="px-6 py-4 whitespace-nowrap">
+                                                <div class="text-sm text-gray-900">
+                                                    {{ $testimonial->content }}
+                                                </div>
+                                            </td>
+                                            <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                                <a href="{{ route('testimonials.edit', $testimonial) }}" class="text-indigo-600 hover:text-indigo-900">{{ __('Edit') }}</a>
+                                                <form method="post" action="{{ route('testimonials.destroy', $testimonial) }}">
+                                                    @csrf
+                                                    @method('delete')
+                                                    <a href="javascript:void(0)" onclick="if(confirm('Anda yakin ingin menghapus testimonial ini?')) event.target.parentElement.submit()" class="text-red-600 hover:text-red-900">{{ __('Delete') }}</a>
+                                                </form>
+                                            </td>
+                                        </tr>
+                                    @empty
+                                        <tr>
+                                            <td colspan="5" class="px-6 py-4 whitespace-nowrap text-center text-sm font-medium">
+                                                {{ __('No records found.') }}
+                                            </td>
+                                        </tr>
+                                    @endforelse
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                {{ $testimonials->links() }}
+            </div>
+        </div>
+    </div>
+</x-app-layout>
