3213929149ea3aed5f1004424d288e460c6b9725
Bagaskara Wisnu Gunawan <bagaswisnu07@gmail.com>
2020-12-02 01:35:30 +0700

    feat: menu

--- resources/views/navigation-dropdown.blade.php
+++ resources/views/navigation-dropdown.blade.php
@@ -25,8 +25,42 @@
                         {{ __('Schedules') }}
                     </x-jet-nav-link>
                     <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
+                        Tiket
+                    </x-jet-nav-link>
+                    <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Settings') }}
                     </x-jet-nav-link>
+                    <div class="inline-flex items-center px-1 pt-1">
+                        <x-jet-dropdown align="right">
+                            <x-slot name="trigger">
+                                <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
+                                    <div>Frontend</div>
+                                    <div class="ml-1">
+                                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                        </svg>
+                                    </div>
+                                </button>
+                            </x-slot>
+                            <x-slot name="content">
+                                <x-jet-dropdown-link href="{{ route('routes.index') }}">
+                                    Galeri
+                                </x-jet-dropdown-link>
+                                <x-jet-dropdown-link href="{{ route('routes.index') }}">
+                                    Promo
+                                </x-jet-dropdown-link>
+                                <x-jet-dropdown-link href="{{ route('routes.index') }}">
+                                    Fasilitas
+                                </x-jet-dropdown-link>
+                                <x-jet-dropdown-link href="{{ route('routes.index') }}">
+                                    Post
+                                </x-jet-dropdown-link>
+                                <x-jet-dropdown-link href="{{ route('routes.index') }}">
+                                    Testimonial
+                                </x-jet-dropdown-link>
+                            </x-slot>
+                        </x-jet-dropdown>
+                    </div>
                 </div>
             </div>
 
