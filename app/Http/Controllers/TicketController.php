3c7faf66d89d849d1bb58aa4ae1e90cb02992ddc
Bagaskara Wisnu Gunawan <bagaswisnu07@gmail.com>
2020-12-02 15:07:15 +0700

    feat: tickets page

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Ticket;
 use Illuminate\Http\Request;
 
 class TicketController extends Controller
 {
     public function index()
     {
-        return view('dashboard');
+        $tickets = Ticket::with('route', 'schedule')->latest()->paginate();
+
+        return view('tickets.index', compact('tickets'));
     }
 }
